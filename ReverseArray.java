42cd56e1fde9b15c8a03482b91366c737b7c4cee
vinod0609600 <sadulavinod06@gmail.com>
2025-04-15 01:10:25 +0530

    Solved DSA Problem:FindingDuplicates

--- ReverseArray.java
+++ ReverseArray.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class ReverseArray {
     public static void main(String[]args){
-
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int []arr=new int[n];
