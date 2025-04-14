3d210dd738f4bcf4d4cc63dbd40dcdea95831337
vinod0609600 <sadulavinod06@gmail.com>
2025-04-15 00:06:45 +0530

    Solved DSA problem: Find max element

---
+++ MaxElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class MaxElement{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(arr[i],max);
+        }
+        System.out.println("maximum element is:"+max);
+        sc.close();
+    }
+}
