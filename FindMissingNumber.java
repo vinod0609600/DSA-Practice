78913f0844ecc6bb72c2a7df96831ee1cc153753
vinod0609600 <sadulavinod06@gmail.com>
2025-04-15 00:48:16 +0530

    Solved DSA problem:FindMissingNumber

---
+++ FindMissingNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FindMissingNumber {
+     public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int xor1=0;
+        int xor2=0;5
+        for(int i=0;i<n;i++){
+            xor1^=arr[i];
+            xor2^=(i+1);
+        }
+        xor2^=N;
+        System.out.println(xor1^xor2);
+    }
+    
+}
