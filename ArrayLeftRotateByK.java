17229ad3c46b7eec443adc437f1bffa2a97778ae
vinod0609600 <sadulavinod06@gmail.com>
2025-04-15 01:14:00 +0530

    Solved DSA problem:FindingUniqueElement

--- ArrayLeftRotateByK.java
+++ ArrayLeftRotateByK.java
@@ -24,6 +24,7 @@ public class ArrayLeftRotateByK {
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
+        sc.close();
     }
 }
 
---
+++ FindingUniqueElement.java
@@ -0,0 +1,25 @@
+import java.security.KeyStore.Entry;
+import java.util.*;
+public class FindingUniqueElement {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Map<Integer,Integer>Hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            Hm.put(arr[i],Hm.getOrDefault(arr[i],0)+1);
+        }
+        System.out.println("Unique elements");
+        for(Map.Entry<Integer,Integer>entryset:Hm.entrySet()){
+            if(entryset.getValue()<2){
+                System.out.println(entryset.getKey()+" ");
+            }
+            
+        }
+    }
+    
+}
+
