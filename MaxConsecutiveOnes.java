8aa9a16c5246f2859ddcce6483691004d07b0666
vinod0609600 <sadulavinod06@gmail.com>
2025-04-15 19:20:29 +0530

    Solved DSA Problem:MaxConsecutiveOnes

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MaxConsecutiveOnes {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        int max=0;
+        for(int num:arr){
+            if(num==1){
+                count++;
+                max=Math.max(count,max);
+            }else{
+                count=0;
+            }
+        }
+        System.out.println(max);
+    }
+}
